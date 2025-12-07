283cd4b61c74baed51849e7da6fe27b9c3d57a93
eximus <fabio4335@gmail.com>
2025-12-06 23:04:21 -0500

    broadcast command

--- src/net/remote.rs
+++ src/net/remote.rs
@@ -4,69 +4,79 @@ use tokio::{
 };
 
 const DATA_SIZE_LIMIT: usize = 5_000_000;
+const COMMAND_SIZE: usize = 4;
+const COMMAND_DATA_SIZE: usize = 8;
 
 pub async fn process(mut socket: TcpStream) {
-    let mut buffer = [0; 4];
     loop {
+        let mut buffer = [0; COMMAND_SIZE];
         let n = socket
             .read(&mut buffer)
             .await
             .expect("failed to read from socket");
-
-        if n != 4 {
-            println!(
-                "couldn't parse command: {} {buffer:?}",
-                String::from_utf8_lossy(&buffer)
-            );
+        if n == 0 {
             return;
         }
 
-        match &buffer {
-            b"PUBL" => publish_parse(&mut socket).await,
+        if n != COMMAND_SIZE {
+            println!(
+                "couldn't parse command: {} {n} {buffer:?}",
+                String::from_utf8_lossy(&buffer)
+            );
+            socket.write_all(b"NO").await.expect("");
+            return;
+        }
+
+        let result = match &buffer {
+            b"BROD" => broadcast_parse(&mut socket).await,
             _ => {
                 println!("unrecognized command: {}", String::from_utf8_lossy(&buffer));
-                ()
+                Err(())
             }
+        };
+
+        // Send Error message
+        if let Err(_) = result {
+            socket.write_all(b"NO").await.expect("");
         }
-        buffer = [0; 4];
     }
 }
 
-async fn publish_parse(socket: &mut TcpStream) {
-    println!(" > PUBLISH COMMAND");
-    let mut data_size_buffer = [0; 10];
+async fn broadcast_parse(socket: &mut TcpStream) -> Result<(), ()> {
+    println!(" > BROD COMMAND");
+    let mut data_size_buffer = [0; COMMAND_DATA_SIZE + 2];
     let n = socket
         .read(&mut data_size_buffer)
         .await
         .expect("failed to read from socket");
-    if n != 10 {
+    if n != COMMAND_DATA_SIZE + 2 {
         println!("coudln't read lenght");
-        return;
+        return Err(());
     }
 
-    let hex_string = String::from_utf8_lossy(&data_size_buffer[1..9]);
+    let hex_string = String::from_utf8_lossy(&data_size_buffer[1..COMMAND_DATA_SIZE + 1]);
     let data_size =
         usize::from_str_radix(hex_string.trim(), 16).expect("failed to parse hex string");
 
     if data_size > DATA_SIZE_LIMIT {
         println!("data size too large");
         // TODO skip that amount of data and reply with ERR
-        return;
+        return Err(());
     }
 
-    println!(" > PUBLISH COMMAND {data_size}");
+    println!(" > BROD COMMAND {data_size}");
 
     let mut buffer = Vec::new();
     buffer.resize(data_size, 0);
 
     let n = socket
-        .read(&mut buffer)
+        .read_exact(&mut buffer)
         .await
         .expect("failed to read from socket");
     if n != data_size {
         println!("data size didn't match?: {n}");
         // TODO send error
-        return;
+        return Err(());
     }
 
     // TODO do something with the data
@@ -76,7 +86,10 @@ async fn publish_parse(socket: &mut TcpStream) {
         .write_all(b"OK\n")
         .await
         .expect("failed to write into socket");
+    Ok(())
 }
+
+async fn broadcast(data: Vec<u8>) {}
 
 // async fn client() {
 //     let mut socket = TcpStream::connect(OTHER_IP)
