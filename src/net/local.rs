a054d1b8e86e075bbb1661d7fd44d31878dbccc3
Connor B <80982854+connbrack@users.noreply.github.com>
2025-12-06 23:28:28 -0500

    add local broadcast function

--- src/net/local.rs
+++ src/net/local.rs
@@ -1,17 +1,93 @@
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpStream};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
+use crate::net::remote::{broadcast};
+
+const DATA_SIZE_LIMIT: usize = 5_000_000;
+const COMMAND_SIZE: usize = 4;
+const COMMAND_DATA_SIZE: usize = 8;
 
 pub async fn process(mut socket: TcpStream) {
-    let mut buffer = [0; 1024];
-    println!("procc");
-
     loop {
-        socket.write_all(b"HELLO THERE\n").await.expect("");
-        let n = socket.read(&mut buffer).await.expect("failed t");
+        let mut buffer = [0; COMMAND_SIZE];
+        let n = socket
+            .read(&mut buffer)
+            .await
+            .expect("failed to read from socket");
         if n == 0 {
             return;
         }
-        socket.write_all(&buffer[0..n]).await.expect("");
+
+        if n != COMMAND_SIZE {
+            println!(
+                "couldn't parse command: {} {n} {buffer:?}",
+                String::from_utf8_lossy(&buffer)
+            );
+            socket.write_all(b"NO").await.expect("");
+            return;
+        }
+
+        let result = match &buffer {
+            b"PUBL" => broadcast_parse(&mut socket).await,
+            _ => {
+                println!("unrecognized command: {}", String::from_utf8_lossy(&buffer));
+                Err(())
+            }
+        };
+
+        // Send Error message
+        if let Err(_) = result {
+            socket.write_all(b"NO").await.expect("");
+            return;
+        }
     }
+}
+
+async fn broadcast_parse(socket: &mut TcpStream) -> Result<(), ()> {
+    println!(" > BROD COMMAND");
+    let mut data_size_buffer = [0; COMMAND_DATA_SIZE + 2];
+    let n = socket
+        .read(&mut data_size_buffer)
+        .await
+        .expect("failed to read from socket");
+    if n != COMMAND_DATA_SIZE + 2 {
+        println!("coudln't read lenght");
+        return Err(());
+    }
+
+    let hex_string = String::from_utf8_lossy(&data_size_buffer[1..COMMAND_DATA_SIZE + 1]);
+    let data_size =
+        usize::from_str_radix(hex_string.trim(), 16).expect("failed to parse hex string");
+
+    if data_size > DATA_SIZE_LIMIT {
+        println!("data size too large");
+        // TODO skip that amount of data and reply with ERR
+        return Err(());
+    }
+
+    println!(" > BROD COMMAND {data_size}");
+
+    let mut buffer = Vec::new();
+    buffer.resize(data_size, 0);
+
+    let n = socket
+        .read_exact(&mut buffer)
+        .await
+        .expect("failed to read from socket");
+    if n != data_size {
+        println!("data size didn't match?: {n}");
+        // TODO send error
+        return Err(());
+    }
+
+    broadcast(buffer).await;
+
+    socket
+        .write_all(b"OK\n")
+        .await
+        .expect("failed to write into socket");
+    Ok(())
 }
 
 // async fn client() {
