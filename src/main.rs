019d04aa2391f83380d9716842174de3b239989d
Connor B <80982854+connbrack@users.noreply.github.com>
2025-12-10 20:25:30 -0500

    Implemented initial config

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use dotenv::dotenv;
+
 pub mod net;
+pub mod config;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     tokio::spawn(async { net::local_server().await });
     tokio::spawn(async { net::remote_server().await });
     loop {}
--- src/net.rs
+++ src/net.rs
@@ -1,15 +1,17 @@
 use tokio::net::TcpListener;
 
 use std::io;
+use crate::config;
 
 pub mod comm;
 pub mod local;
 pub mod remote;
 
 pub async fn local_server() -> io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:9999")
+    let local_port = config::local_port();
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", local_port))
         .await
-        .expect("Failed binding to port 8080");
+        .expect(&format!("Failed binding to {}", local_port));
     loop {
         let (socket, _) = listener.accept().await?;
         tokio::spawn(async move { local::process(socket).await });
@@ -17,9 +19,10 @@ pub async fn local_server() -> io::Result<()> {
 }
 
 pub async fn remote_server() -> io::Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:8080")
+    let remote_port = config::remote_port();
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", remote_port))
         .await
-        .expect("Failed binding to port 8080");
+        .expect(&format!("Failed binding to port {}", remote_port));
     loop {
         let (socket, _) = listener.accept().await?;
         tokio::spawn(async move { remote::process(socket).await });
