9630188ed37b0c6ed04ebd70d0df6dbc6a9c5c0d
eximus <fabio4335@gmail.com>
2026-01-31 22:56:20 -0500

    feat: broadcast local command over http

--- src/net/local.rs
+++ src/net/local.rs
@@ -1,19 +1,39 @@
+use std::collections::HashMap;
+
 // vim: fdm=indent fdn=1
-// use super::remote::broadcast;
+use super::remote::broadcast;
 use hyper::{Method, Request, Response, StatusCode};
 
 pub async fn process(
     req: Request<hyper::body::Incoming>,
 ) -> Result<Response<String>, hyper::Error> {
-    println!("Received {} request to {}", req.method(), req.uri().path());
-
+    println!("+ {} {}", req.method(), req.uri());
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => Ok(Response::new("Overheard Node\r\n".to_string())),
         (&Method::GET, "/join") => Ok(Response::new("Joined\r\n".to_string())),
+        (&Method::GET, "/broadcast") => broadcast_cmd(req).await,
         _ => {
             let mut response = Response::new("Not Found".to_string());
             *response.status_mut() = StatusCode::NOT_FOUND;
             Ok(response)
         }
     }
+}
+
+pub async fn broadcast_cmd(
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<String>, hyper::Error> {
+    let params = req
+        .uri()
+        .query()
+        .map(|v| {
+            url::form_urlencoded::parse(v.as_bytes())
+                .into_owned()
+                .collect()
+        })
+        .unwrap_or_else(HashMap::new);
+
+    let data = params.get("data").unwrap_or(&String::from("")).clone();
+    let _ = broadcast(data.into_bytes().to_vec()).await;
+    Ok(Response::new("Broadcast sent\r\n".to_string()))
 }
