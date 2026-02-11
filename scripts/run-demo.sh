e95f0be38d3ebc7bee397867a4a997bf0840ec79
eximus <fabio4335@gmail.com>
2026-02-11 09:59:15 -0500

    chore: add small script to link nodes/broadcast

---
+++ scripts/run-demo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker exec overherd-node-1 curl -s localhost:9999/join?peer=overherd-node-2
+docker exec overherd-node-2 curl -s localhost:9999/join?peer=overherd-node-3
+docker exec overherd-node-2 curl -s localhost:9999/broadcast?data=hello
